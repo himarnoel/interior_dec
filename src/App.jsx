80adb37ba0ad1eb936c8fdee1cde6bc6018d1e63
himarnoel <olaimarnoel@gmail.com>
2023-02-01 20:14:14 +0100

    ready to start

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function App() {
+
+
+  return (
+    <>
+      <span className="text-3xl font-bold underline bg-red-600">Hello world!</span>
+    </>
+  );
+}
+
+export default App;
