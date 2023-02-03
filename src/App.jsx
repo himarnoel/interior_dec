f4949612f7869050f78bfcbfd7450f65fe01b129
himarnoel <olaimarnoel@gmail.com>
2023-02-03 21:07:02 +0100

    working on nav bar for lg

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import React from "react";
+import icon from "./assets/icons/Vector.svg";
+import logo from "./assets/Logo.jpg";
 function App() {
-
-
   return (
-    <>
-      <span className="text-3xl font-bold underline bg-red-600">Hello world!</span>
-    </>
+    <div className="lg:max-w-4xl mx-auto">
+      <nav className="2xl:bg-blue-200 mt-2 flex items-center justify-between">
+        <span className="flex items-center gap-y-3">
+          <img src={logo} alt="" className="lg:mr-2 object-contain w-5 " />
+          <h1 className="font-medium text-xl">Interno</h1>
+        </span>
+        <span className="flex w-[500px] justify-around text-xs">
+          <a href="">Home</a>
+          <a href="">Pages</a>
+          <a href="">Services</a>
+          <a href="">Project</a>
+          <a href="">Blog</a>
+          <a href="">Contact</a>
+          <img src={icon} className="object-contain w-4" />
+        </span>
+      </nav>
+    </div>
   );
 }
 
