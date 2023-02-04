26cb3a8128582f5e01d059a1842dccc46777117d
himarnoel <olaimarnoel@gmail.com>
2023-02-04 18:31:10 +0100

    added icon

--- src/Components/Herox.jsx
+++ src/Components/Herox.jsx
@@ -5,12 +5,12 @@ import { BsArrowRight } from "react-icons/bs";
 const Herox = () => {
   return (
     <div>
-      <div className="md:mt-[4.2rem] mt-[3rem] lg:mt-2">
+      <div className="md:mt-[4.2rem] mt-[3rem] lg:mt-0">
         <img
           src={Hero}
-          className="w-screen h-[30rem]  object-cover rounded-bl-[100px] "
+          className="w-screen h-[30rem] lg:h-full  object-cover rounded-bl-[100px]"
         />
-        <div className="absolute top-28 md:top-[8rem] xl:top-[13rem]  xl:left-[14rem] lg:left-[9rem] md:left-[2.5rem] left-4   w-[15rem] sm:w-80 h-60   flex flex-col justify-around items-start ">
+        <div className="absolute top-28 md:top-[8rem] xl:top-[15rem]  xl:left-[14rem] lg:left-[9rem] md:left-[2.5rem] left-4   w-[15rem] sm:w-80 h-60   flex flex-col justify-around items-start ">
           <h1 className="sm:text-5xl text-4xl">
             {" "}
             <p>Let your Home</p>
--- src/Components/Info.jsx
+++ src/Components/Info.jsx
@@ -1,27 +1,28 @@
 import React from "react";
 import photo from "../assets/Photo.jpg";
 import { BsArrowRight } from "react-icons/bs";
+import { HiOutlinePhone } from "react-icons/hi";
 const Info = () => {
   return (
     <div>
       {" "}
       <div className="w-full b flex-col-reverse mt-10 grid md:grid-cols-2  md:justify-items-end xl:px-[14rem] lg:px-[9rem] px-[1rem] mb-96 g">
-        <div className="flex flex-col justify-around items-start  md:text-left text-center   lg:w-full h-[26rem] lg:h-full mx-auto">
+        <div className="flex flex-col justify-around md:justify-between items-start  md:text-left text-center   lg:w-full h-[26rem] lg:h-full mx-auto">
           {" "}
           <h1 className=" md:text-4xl  text-xl lg:text-left  w-full">
             We Create The Art <br /> Of Stylish Living <br /> Stylishly
           </h1>
-          <p className="font-jost md:text-sm lg:text-base text-xs leading-6 text-center lg:text-left ">
+          <p className="font-jost md:text-sm lg:text-base text-xs leading-6 text-center md:text-left ">
             It is a long established fact that a reader will{" "}
-            <br className="hidden lg:block" /> be distracted by the of readable
-            content of a page <br className="hidden lg:block" /> when lookings
+            <br className="hidden md:block" /> be distracted by the of readable
+            content of a page <br className="hidden md:block" /> when lookings
             at its layouts the points of using{" "}
-            <br className="hidden lg:block" /> that it has a more-or-less
+            <br className="hidden md:block" /> that it has a more-or-less
             normal.
           </p>
           <span className="flex ">
-            <span className="rounded-full h-10 flex justify-center items-center w-10 bg-[#F4F0EC] mr-3 ">
-              1
+            <span className="rounded-full h-12 flex justify-center items-center w-12 bg-[#F4F0EC] mr-3 text-[#CDA274] text-xl ">
+              <HiOutlinePhone />
             </span>
             <span className="flex flex-col font-jost">
               {" "}
@@ -29,14 +30,14 @@ const Info = () => {
               <p className="font-light">Call Us Anytime</p>
             </span>
           </span>
-          <button className="p-3 text-white shadow-lg bg-[#292F36] font-jost flex items-center rounded-md">
+          <button className="p-3 text-white shadow-lg bg-[#292F36] font-jost flex items-center rounded-xl">
             Get Free Estimate <BsArrowRight className="ml-2 text-[#CDA274]" />
           </button>
         </div>
         <img
           src={photo}
           alt=""
-          className="rounded-tr-[12rem] rounded-bl-[6rem] object-contain px md:w-[24rem] mt-1 md:mt-0 mx-auto"
+          className="rounded-tr-[12rem] rounded-bl-[6rem] object-contain px  mt-1 md:mt-0 mx-auto"
         />
       </div>
     </div>
