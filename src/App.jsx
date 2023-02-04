dd5ce0471f352c91891661c5477874bfc579bddc
himarnoel <olaimarnoel@gmail.com>
2023-02-04 16:20:49 +0100

    added new component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Navbar from "./Components/Navbar";
 import Herox from "./Components/Herox";
 import Actions from "./Components/Actions";
+import Info from "./Components/Info";
 
 function App() {
   const [change, setchange] = useState(false);
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Herox />
       <Actions />
+      <Info />
     </div>
   );
 }
---
+++ src/Components/Info.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Info = () => {
+  return (
+    <div> <div className="w-full bg-fuchsia-500 h-64 mt-3 grid  xl:px-[14rem] lg:px-[9rem] px-[1rem] ">
+    <div>
+      {" "}
+      <p>We Create The Art Of Stylish Living Stylishly</p>
+      <p>
+        It is a long established fact that a reader will be distracted by
+        the of readable content of a page when lookings at its layouts the
+        points of using that it has a more-or-less normal.
+      </p>
+    </div>
+    <img src="" alt="" />
+  </div></div>
+  )
+}
+
+export default Info
