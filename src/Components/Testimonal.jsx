8ae577acb541be0a4c176fcb2dce98f36fcbf18d
himarnoel <olaimarnoel@gmail.com>
2023-02-06 21:47:47 +0100

    done with main card

--- src/Components/Testimonal.jsx
+++ src/Components/Testimonal.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Testimonal = () => {
   return (
-    <div>Testimonal</div>
-  )
-}
+    <div>
+      <div>
+        <img
+          class="inline-block h-20 w-20 rounded-full ring-1 ring-transparent object-contain"
+          src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Testimonal
+export default Testimonal;
--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -3,7 +3,14 @@ import React from "react";
 const Testimonials = () => {
   return (
     <div>
-      <div className="w-full bg-fuchsia-400 p-20">sdsdafsd</div>
+      <div className="w-full   xl:px-[14rem] lg:px-[9rem] px-[1rem] bg-white justify-center items-center flex mb-20">
+        <div className="rounded-[3rem] h-[18rem] w-full bg-[#F4F0EC] text-center">
+          <p className="text-2xl mt-6">
+            What the People Thinks <br /> About Us
+          </p>
+          
+        </div>
+      </div>
     </div>
   );
 };
