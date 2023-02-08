4c2f318bfc816eaaf34e81af8f8ca9e0b30bcf23
himarnoel <olaimarnoel@gmail.com>
2023-02-08 12:57:32 +0100

    added image

---
+++ src/Components/Brands.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import svg1 from "../assets/svgs/01.svg";
+import svg2 from "../assets/svgs/02.svg";
+import svg3 from "../assets/svgs/03.svg";
+import svg4 from "../assets/svgs/03.svg";
+const Brands = () => {
+  return (
+    <div>
+        <div className='flex w-full'>
+            <img src={svg1} alt="" />  <img src={svg2} alt="" />  <img src={svg3} alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Brands
