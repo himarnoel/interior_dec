1a4163a64da021538bbe434266f5f0de013b530c
himarnoel <olaimarnoel@gmail.com>
2023-02-08 15:15:58 +0100

    remove

--- src/Components/ImageProject.jsx
+++ src/Components/ImageProject.jsx
@@ -3,12 +3,20 @@ import React from "react";
 const ImageProject = (props) => {
   return (
     <div>
-      <div className="mt-10">
+      <div className="mt-0">
         <img
           src={props.image}
-          className={`object-contain w-[22rem]  ${props.radius} `}
+          className={`object-contain w-full  ${props.radius} `}
         />
-        
+        <div className="w-full flex">
+          <span className="flex flex-col">
+            <h4 className="">Modern Kitchan</h4>
+            <p className="w-10">Decor / Artchitecture</p>
+          </span>
+          <span className="h-5 w-5 bg-[#F4F0EC]">
+            
+          </span>
+        </div>
       </div>
     </div>
   );
