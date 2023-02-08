f856826ca52f870dbd45a3d93ff6687815f92c03
himarnoel <olaimarnoel@gmail.com>
2023-02-08 16:59:13 +0100

    removed bugs

--- src/Components/Info.jsx
+++ src/Components/Info.jsx
@@ -6,7 +6,7 @@ const Info = () => {
   return (
     <div>
       {" "}
-      <div className="w-full b flex-col-reverse  grid sm:grid-cols-2  md:justify-items-end  xl:px-[14rem] lg:px-[9rem] sm:px-[1.3rem]  px-[1.3rem] md:px-[2.5rem]">
+      <div className="w-full b flex-col-reverse  grid sm:grid-cols-2  md:justify-items-end  xl:px-[14rem] lg:px-[9rem] sm:px-[1.3rem]  px-[1.3rem] md:px-[2.5rem] mt-10 md:mt-[10rem]">
         <div className="flex flex-col justify-around sm:justify-between items-start  sm:text-left text-center   lg:w-full h-[26rem] lg:h-full mx-auto">
           {" "}
           <h1 className=" sm:text-4xl  text-xl lg:text-left  w-full">
