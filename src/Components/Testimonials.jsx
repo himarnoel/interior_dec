16a0ab1a3ed0e0a6e17bad31bc09cdaf4bb39691
himarnoel <olaimarnoel@gmail.com>
2023-02-08 12:54:26 +0100

    size

--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -6,7 +6,7 @@ const Testimonials = () => {
     <div>
       <div className="w-full   xl:px-[14rem] lg:px-[9rem] px-[1rem] bg-white justify-center items-center flex mb-[10rem]">
         <div className=" rounded-xl lg:rounded-[3rem] w-full bg-[#F4F0EC] text-center">
-          <p className="text-2xl lg:text-3xl mt-8 mb-4">
+          <p className="text-2xl lg:text-[1.58rem] mt-8 mb-4">
             What the People Thinks <br /> About Us
           </p>
           <div className="grid sm:grid-cols-3 place-items-center justify-evenly gap-4  px-4 md:px-6 lg:px-6 pb-10">
