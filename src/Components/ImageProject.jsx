9f1e09cab2b10ea421587f69b3cf32d0103028f5
himarnoel <olaimarnoel@gmail.com>
2023-02-08 14:16:21 +0100

    image added

--- src/Components/ImageProject.jsx
+++ src/Components/ImageProject.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-
+import image from "../assets/mid/Photo1.png";
 const ImageProject = () => {
   return (
     <div>
-      <div></div>
+      <div>
+        <img src={image} alt="" className="object-contain w-10"/>
+      </div>
     </div>
   );
 };
