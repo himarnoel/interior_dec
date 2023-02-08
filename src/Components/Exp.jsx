41236d8fc3407b358b6fa1b741371646ed68a7f9
himarnoel <olaimarnoel@gmail.com>
2023-02-08 16:49:49 +0100

    done

--- src/Components/Exp.jsx
+++ src/Components/Exp.jsx
@@ -1,9 +1,53 @@
 import React from "react";
 
 const Exp = () => {
-  return <div className="w-full bg-[#F4F0EC]">
-    
-  </div>;
+  return (
+    <div className="w-full  bg-[#F4F0EC]   xl:px-[14rem] lg:px-[9rem] sm:px-[1.3rem] md:px-[2.5rem] px-[3rem] mt-[10rem] mb-64">
+      <div className=" w-full divide-x divide-dashed divide-cyan-700 md:py-20 py-5">
+        <div class="divide-x flex flex-col sm:flex-row items-center  justify-evenly  w-full">
+          <div className="flex flex-col items-center text-center">
+            <p className="text-[#CDA274] text-[3.5rem]">12</p>
+            <p
+              className="mt-2 font-jost
+            "
+            >
+              Years Of Experience
+            </p>
+          </div>
+          <div className=" w-2  border-[#CDA274] h-24"></div>
+          <div className="flex flex-col items-center text-center ">
+            <p className="text-[#CDA274] text-[3.5rem]">85</p>
+            <p
+              className="mt-2 font-jost
+            "
+            >
+              Success Project
+            </p>
+          </div>
+          <div className=" w-2  border-[#CDA274] h-24"></div>
+          <div className="flex flex-col items-center text-center">
+            <p className="text-[#CDA274] text-[3.5rem]">15</p>
+            <p
+              className="mt-2 font-jost
+            "
+            >
+              Active Project
+            </p>
+          </div>
+          <div className=" w-2  border-[#CDA274] h-24"></div>
+          <div className="flex flex-col items-center text-center">
+            <p className="text-[#CDA274] text-[3.5rem]">95</p>
+            <p
+              className="mt-2 font-jost
+            "
+            >
+              Happy CUstomers
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Exp;
