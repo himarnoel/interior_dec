cef7b920e6f156bdb23082a0e2d47d7dab26e9ee
himarnoel <olaimarnoel@gmail.com>
2023-02-08 13:38:41 +0100

    made changes

--- src/Components/Brands.jsx
+++ src/Components/Brands.jsx
@@ -7,13 +7,33 @@ import svg5 from "../assets/svgs/05.svg";
 
 const Brands = () => {
   return (
-    <div className="flex w-full xl:px-[14rem] lg:px-[9rem] px-[1rem] mt-10 md:mt-[10rem] mb-[20rem]  bg-fuchsia-400">
-      <div className="grid sm:grid-cols-5  md:grid-cols-5 sm:h-full sm:justify-items-center h-96 w-full  bg-blue-500 items-center justify-center">
-        <img src={svg1} alt="" className="object-contain w-[5.2rem] sm:w-[3.5rem]" />
-        <img src={svg2} alt="" className="object-contain w-[5.2rem] sm:w-[3.5rem]" />
-        <img src={svg3} alt="" className="object-contain w-[5.2rem] sm:w-[3.5rem]" />
-        <img src={svg4} alt="" className="object-contain w-[5.2rem] sm:w-[3.5rem]" />
-        <img src={svg5} alt="" className="object-contain w-[5.2rem] sm:w-[3.5rem]" />
+    <div className="flex w-full xl:px-[15rem] lg:px-[9rem] sm:px-[4rem] px-[3rem] mt-10 md:mt-[10rem] mb-[20rem]  ">
+      <div className="grid sm:grid-cols-5  md:grid-cols-5 sm:h-full sm:justify-items-center h-96 w-full items-center justify-center">
+        <img
+          src={svg1}
+          alt=""
+          className="object-contain w-[5.2rem] sm:w-[3.5rem] md:w-[5.2rem]"
+        />
+        <img
+          src={svg2}
+          alt=""
+          className="object-contain w-[5.2rem] sm:w-[3.5rem] md:w-[5.2rem]"
+        />
+        <img
+          src={svg3}
+          alt=""
+          className="object-contain w-[5.2rem] sm:w-[3.5rem] md:w-[5.2rem]"
+        />
+        <img
+          src={svg4}
+          alt=""
+          className="object-contain w-[5.2rem] sm:w-[3.5rem] md:w-[5.2rem]"
+        />
+        <img
+          src={svg5}
+          alt=""
+          className="object-contain w-[5.2rem] sm:w-[3.5rem] md:w-[5.2rem]"
+        />
       </div>
     </div>
   );
--- src/Components/Testimonal.jsx
+++ src/Components/Testimonal.jsx
@@ -3,16 +3,16 @@ import React from "react";
 const Testimonal = (props) => {
   return (
     <div>
-      <div className="bg-white w-full  h-64 sm:h-[19rem] md:h-52 lg:h-[17rem] xl:h-[14rem] rounded-xl  px-6 pt-5">
+      <div className="bg-white w-full  h-64 sm:h-[17rem] md:h-52 lg:h-[17rem] xl:h-[14rem] rounded-xl  px-6 sm:px-4 md:px-6 pt-5">
         <div className="flex items-center-center">
           {" "}
           <img
-            class="inline-block h-10 w-10 rounded-full ring-1 ring-transparent object-contain mr-4 "
+            class="inline-block h-10 w-10 sm:h-8 sm:w-8 md:h-10 md:w-10 rounded-full ring-1 ring-transparent object-contain mr-4 "
             src={props.image}
             alt=""
           />
           <span className="flex flex-col text-left">
-            <span className="text-base">{props.name} </span>
+            <span className="text-base sm:text-sm md:text-base ">{props.name} </span>
             <span className="font-jost text-sm text-[#4D5053]">
               {props.location}
             </span>
