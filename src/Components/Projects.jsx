88c265d5804c5c64866660753a9ceae73846f65d
himarnoel <olaimarnoel@gmail.com>
2023-02-08 14:13:06 +0100

    command

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,15 +1,21 @@
 import React from "react";
+import ImageProject from "./ImageProject";
 
 const Projects = () => {
   return (
     <div>
-      <div className="flex w-full xl:px-[15rem] lg:px-[9rem] sm:px-[4rem] px-[3rem] mt-10 md:mt-[10rem] mb-32 ">
-        <div className="w-full ">
+      <div className=" w-full xl:px-[15rem] lg:px-[9rem] sm:px-[4rem] px-[3rem] mt-10 md:mt-[10rem] mb-32 bg-blue-300">
+        <div className="w-full bg-red-300 ">
           <p className="text-xl text-center">Follow Our Projects</p>
           <p className="text-center font-jost ">
             It is a long established fact that a reader will be distracted by
             the of readable content of page lookings at its layouts points.
           </p>
+        </div>
+        <div className="w-full">
+          <ImageProject />
+          <ImageProject />
+          <ImageProject />
         </div>
       </div>
     </div>
