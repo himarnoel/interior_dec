c4b01bdefa90367d4ea9e86329676870a2d75134
himarnoel <olaimarnoel@gmail.com>
2023-02-09 19:34:59 +0100

    edited a part of ui

--- src/Components/Exp.jsx
+++ src/Components/Exp.jsx
@@ -6,7 +6,7 @@ const Exp = () => {
       <div className=" w-full divide-x divide-dashed divide-cyan-700 md:py-20 py-5">
         <div class="divide-x flex flex-col sm:flex-row items-center  justify-evenly  w-full">
           <div className="flex flex-col items-center text-center">
-            <p className="text-[#CDA274] text-[3.5rem]">12</p>
+            <p className="text-[rgb(205,162,116)] text-[3.5rem]">12</p>
             <p
               className="mt-2 font-jost
             "
@@ -14,7 +14,7 @@ const Exp = () => {
               Years Of Experience
             </p>
           </div>
-          <div className=" w-2  bg-[#CDA274] h-24"></div>
+          <div className=" w-[0.13rem]  bg-[#CDA274] h-24"></div>
           <div className="flex flex-col items-center text-center ">
             <p className="text-[#CDA274] text-[3.5rem]">85</p>
             <p
@@ -24,7 +24,7 @@ const Exp = () => {
               Success Project
             </p>
           </div>
-          <div className=" w-2  bg-[#CDA274] h-24"></div>
+          <div className="w-[0.13rem]   bg-[#CDA274] h-24"></div>
           <div className="flex flex-col items-center text-center">
             <p className="text-[#CDA274] text-[3.5rem]">15</p>
             <p
@@ -34,7 +34,7 @@ const Exp = () => {
               Active Project
             </p>
           </div>
-          <div className=" w-2  bg-[#CDA274] h-24"></div>
+          <div className="w-[0.13rem]   bg-[#CDA274] h-24"></div>
           <div className="flex flex-col items-center text-center">
             <p className="text-[#CDA274] text-[3.5rem]">95</p>
             <p
