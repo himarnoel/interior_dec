6dea21cb6a62f34626469d31fff2cacfc28dbcf5
himarnoel <olaimarnoel@gmail.com>
2023-02-10 02:57:57 +0100

    removed bug

--- src/Components/Foot.jsx
+++ src/Components/Foot.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Foot = () => {
-  return <div>Foot</div>;
+  return (
+    <div>
+      <div className="w-full  xl:px-[14rem] lg:px-[9rem] sm:px-[1.3rem] md:px-[2.5rem] px-[3rem] mt-[10rem] mb-64"></div>
+    </div>
+  );
 };
 
 export default Foot;
