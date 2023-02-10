da8e8f5bf14e5b166875fda72e1b0f4d595ceb68
himarnoel <olaimarnoel@gmail.com>
2023-02-10 03:43:03 +0100

    added bg

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -4,8 +4,8 @@ import { BsArrowRight } from "react-icons/bs";
 const News = () => {
   return (
     <div>
-      <div className="w-full  bg-[#F4F0EC]   xl:px-[14rem] lg:px-[9rem] sm:px-[1.3rem] md:px-[2.5rem] px-[3rem] mt-[10rem] ">
-        <div className=" text-center">
+      <div className="w-full     xl:px-[14rem] lg:px-[9rem] sm:px-[1.3rem] md:px-[2.5rem] px-[3rem] mt-[10rem] ">
+        <div className=" text-center bg-[#F4F0EC]">
           <p className="text-xl text-center">Articles & News</p>
          
         </div>
