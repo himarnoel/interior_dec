a4d8b1b7482ba10065817e508d05c5f6fa6538af
himarnoel <olaimarnoel@gmail.com>
2023-02-10 03:03:14 +0100

    working on footer

--- src/Components/Foot.jsx
+++ src/Components/Foot.jsx
@@ -3,7 +3,18 @@ import React from "react";
 const Foot = () => {
   return (
     <div>
-      <div className="w-full  xl:px-[14rem] lg:px-[9rem] sm:px-[1.3rem] md:px-[2.5rem] px-[3rem] mt-[10rem] mb-64"></div>
+      <footer className="w-full bg-blue-500 h-[20rem]  xl:px-[14rem] lg:px-[9rem] sm:px-[1.3rem] md:px-[2.5rem] px-[3rem] mt-[10rem] ">
+        <div className="grid md:grid-cols-2 lg:grid-cols-4">
+          <div className="flex flex-col ">
+            <span className="flex">
+              <img src="" alt="" />
+            </span>
+          </div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </footer>
     </div>
   );
 };
